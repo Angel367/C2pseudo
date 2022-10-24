b4b7dff9bab7e7a61592877bb0a1eac35d64520d
Волков Егор <egor.volkov20038@gmail.com>
2022-10-24 20:32:48 +0300

    raw worked

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.body.onclick = function (e) {
         let inpData = document.getElementById("input").value;
         inpData = inpData.replaceAll("int", "ЦЕЛ");
         inpData = inpData.replaceAll("if", "ЕСЛИ");
-        //console.log(inpData);
+        inpData = inpData.replaceAll("else", "ИНАЧЕ");
         document.getElementById("output").value = inpData;
     }
 }
