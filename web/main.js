d60a8c1146e92351238e197dff56d722daaa6fb3
Волков Егор <egor.volkov20038@gmail.com>
2022-10-24 20:25:00 +0300

    raw worked

--- web/main.js
+++ web/main.js
@@ -1,11 +1,10 @@
 document.body.onclick = function (e) {
     let target = e.target || e.srcElement;
     if ((target.id === "butt")){
-
-        let inpData = document.getElementById("a").value;
+        let inpData = document.getElementById("input").value;
         inpData = inpData.replaceAll("int", "ЦЕЛ");
         inpData = inpData.replaceAll("if", "ЕСЛИ");
         console.log(inpData);
-        document.getElementById("a2").value = inpData;
+        document.getElementById("output").value = inpData;
     }
 }
