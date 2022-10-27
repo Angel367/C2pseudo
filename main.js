0e4b68273ea545e69ac52fa78df481f47742fb23
Волков Егор <egor.volkov20038@gmail.com>
2022-10-27 12:23:06 +0300

    Добавил replace`ов

--- main.js
+++ main.js
@@ -3,8 +3,28 @@ document.body.onclick = function (e) {
     if ((target.id === "butt")){
         let inpData = document.getElementById("input").value;
         inpData = inpData.replaceAll("int", "ЦЕЛ");
+        inpData = inpData.replaceAll("string", "СТРОКА");
+        inpData = inpData.replaceAll("double", "ВЕЩ");
+        inpData = inpData.replaceAll("bool", "ЛОГ");
+        inpData = inpData.replaceAll("char", "СИМВ");
+        // Логические значения
+        inpData = inpData.replaceAll("true", "ПРАВДА");
+        inpData = inpData.replaceAll("false", "ЛОЖЬ");
+        // Условные операторы
         inpData = inpData.replaceAll("if", "ЕСЛИ");
+        inpData = inpData.replaceAll("else if", "ТАКЖЕ ЕСЛИ");
         inpData = inpData.replaceAll("else", "ИНАЧЕ");
+        // Операторы циклов
+        inpData = inpData.replaceAll("while", "ПОКА");
+        inpData = inpData.replaceAll("do", "ДЕЛ");
+        // Отдельные символы
+        inpData = inpData.replaceAll(">", "БОЛЬШЕ");
+        inpData = inpData.replaceAll(">=", "БОЛЬШЕ ИЛИ РАВНО");
+        inpData = inpData.replaceAll("<", "МЕНЬШЕ");
+        inpData = inpData.replaceAll("<=", "МЕНЬШЕ ИЛИ РАВНО");
+        inpData = inpData.replaceAll("=", "←");
+        // Какие-то операторы
+        inpData = inpData.replaceAll("return", "ВЕРНУТЬ");
         document.getElementById("output").value = inpData;
     }
 }
